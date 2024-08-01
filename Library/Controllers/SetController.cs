83f09960dbf2664a77af64fca61dcc33b646ab69
benitalker <benitalker00@gmail.com>
2024-08-01 12:04:11 +0300

    first commit

---
+++ Library/Controllers/SetController.cs
@@ -0,0 +1,38 @@
+﻿using Library.Models;
+using Library.Service;
+using Library.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class SetController : Controller
+    {
+        private readonly ISetService _setService;
+        public SetController(ISetService setService)
+        {
+            _setService = setService;
+        }
+        public IActionResult Index(long id)
+        {
+            ViewBag.Id = id;
+            return View(_setService.GetAllSetsById(id));
+        }
+        
+        public IActionResult Create(long id)
+        {
+
+            return View(new SetVM() { });
+        }
+        [HttpPost]
+        public IActionResult Create(SetVM set)
+        {
+            _setService.CreateSet(set);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(long id)
+        {
+            _setService.DeleteSet(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Library/Service/ISetService.cs
@@ -0,0 +1,12 @@
+﻿using Library.Models;
+using Library.ViewModel;
+
+namespace Library.Service
+{
+    public interface ISetService
+    {
+        List<SetModel> GetAllSetsById(long id);
+        void CreateSet(SetVM newSet);
+        void DeleteSet(long id);
+    }
+}
